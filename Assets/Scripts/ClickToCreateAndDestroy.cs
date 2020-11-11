3fc3d520076cdde8bfbfaccf7ec5fc8fd584bbc3
Marcelo Cohen <mflashbr@gmail.com>
2020-11-10 22:23:44 -0300

    Exemplos de Instantiate/Destroy, áudio/mixer, co-rotinas, delegates/eventos.

--- Assets/Scripts/ClickToCreateAndDestroy.cs
+++ Assets/Scripts/ClickToCreateAndDestroy.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ClickToCreateAndDestroy : MonoBehaviour
 {
+    public GameObject obj;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +14,40 @@ public class ClickToCreateAndDestroy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Pode ser colocado dentro do if (mais eficiente)
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow);
 
+        bool botao1 = Input.GetMouseButtonDown(0);
+        bool botao2 = Input.GetMouseButtonDown(1);
+        if (botao1 || botao2)
+        {
+            RaycastHit rayCastInfo;
+            if (Physics.Raycast(ray, out rayCastInfo))
+            {
+                if (botao1 && rayCastInfo.transform.tag == "plane")
+                {
+                    Vector3 pos = rayCastInfo.point;
+                    pos.y = 0.5f;
+                    Instantiate(obj, pos, Quaternion.identity);
+                }
+                else if (botao2 && rayCastInfo.transform.tag == "cube")
+                {
+                    //Destroy(rayCastInfo.transform.gameObject);
+                    StartCoroutine(DestroyAfterSeconds(rayCastInfo.transform.gameObject, 3));
+                }
+                print(rayCastInfo.transform.gameObject.name
+                 + " - " + rayCastInfo.point);
+            }
+        }
+    }
+
+    IEnumerator DestroyAfterSeconds(GameObject obj, float seconds)
+    {
+        print("Na Corotina");
+        yield return new WaitForSeconds(seconds);
+
+        Destroy(obj);
+        print("Fim da Corotina");
     }
 }
---
+++ Assets/Scripts/TurnColor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnColor : MonoBehaviour
+{
+    void OnEnable()
+    {
+        EventManager.OnClicked += TurnColorNow;
+    }
+
+    void OnDisable()
+    {
+        EventManager.OnClicked -= TurnColorNow;
+    }
+
+    void TurnColorNow()
+    {
+        Color color = new Color(UnityEngine.Random.value,
+        UnityEngine.Random.value, UnityEngine.Random.value);
+        GetComponent<Renderer>().material.color = color;
+    }
+}
